3ff8e8c78256c13a9999ca3ff42f4802101cb3ad
Sankalpgupta74 <sankalp.gupta_cs.aiml23@gla.ac.in>
2026-07-21 10:12:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (62.11%) - LeetSync

---
+++ 66-plus-one/plus-one.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length-1;
+        for(int i=n;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
+            }
+            else{
+                digits[i]+=1;
+                return digits;
+            }
+
+        }
+        int newa[]=new int [digits.length+1];
+        newa[0]=1;
+        return newa;
+        
+    }
+}
